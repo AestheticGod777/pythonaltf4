a66ec69843a4f30f50ceb4625548aa0fd4ad47e2
HBoldi777 <boldi240905@yahoo.com>
2021-05-24 19:46:44 +0200

    Játéknak mostantól van vége.

--- jatek.py
+++ jatek.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import random
 
 
@@ -19,9 +20,14 @@ kaja = [0, 0]
 uj_kaja = True
 uj_darab = [0, 0]
 pontszam = 0
+jatek_vege = False
+kattintva = False
 
 #Betűtípus
 betutipus = pygame.font.SysFont(None, 40)
+
+#Doboz
+ujra_doboz = Rect(screen_width // 2 - 100, screen_height // 2, 220, 60)
 
 #Színek
 belso_resz = (50, 175, 25)
@@ -44,8 +50,29 @@ def hatter_kirajzolasa():
 def pontszam_kirajzolasa():
     pontszam_txt = "Pontszám: " + str(pontszam)
     pontszam_img = betutipus.render(pontszam_txt, True, (100, 50, 255))
-    screen.blit(pontszam_img, (0,0))
+    screen.blit(pontszam_img, (0, 0))
 
+def jateknak_vege(jatek_vege):
+
+    fej_szama = 0
+    for segment in kigyo_helye:
+        if kigyo_helye[0] == segment and fej_szama > 0:
+            jatek_vege = True
+        fej_szama += 1
+
+    if kigyo_helye[0][0] < 0 or kigyo_helye[0][0] > screen_width or kigyo_helye[0][1] < 0 or kigyo_helye[0][1] > screen_height:
+        jatek_vege = True
+    return jatek_vege
+
+def jatek_vege_kirajzolasa():
+    vege_txt = "Vége a játéknak!"
+    vege_img = betutipus.render(vege_txt, True, (0, 0, 250))
+    screen.blit(vege_img, (screen_width // 2 - 100, screen_height // 2 - 80))
+
+    ujra_txt = "Újra kezded?"
+    ujra_img = betutipus.render(ujra_txt, True, (0, 0, 250))
+    pygame.draw.rect(screen, (255, 255, 255), ujra_doboz)
+    screen.blit(ujra_img, (screen_width // 2 - 80, screen_height // 2 + 15))
 
 run = True
 while run:
@@ -91,30 +118,52 @@ while run:
         if irany == 4:
             uj_darab[0] += cella_merete
 
-
         kigyo_helye.append(uj_darab)
 
         pontszam += 1
+    if jatek_vege == False:
+        if kigyo_frissit > 170:
+            kigyo_frissit = 0
+            kigyo_helye = kigyo_helye[-1:] + kigyo_helye[:-1]
+            #fel
+            if irany == 1:
+                kigyo_helye[0][0] = kigyo_helye[1][0]
+                kigyo_helye[0][1] = kigyo_helye[1][1] - cella_merete
+            #jobbra
+            if irany == 2:
+                kigyo_helye[0][1] = kigyo_helye[1][1]
+                kigyo_helye[0][0] = kigyo_helye[1][0] + cella_merete
+            #le
+            if irany == 3:
+                kigyo_helye[0][0] = kigyo_helye[1][0]
+                kigyo_helye[0][1] = kigyo_helye[1][1] + cella_merete
+            #balra
+            if irany == 4:
+                kigyo_helye[0][1] = kigyo_helye[1][1]
+                kigyo_helye[0][0] = kigyo_helye[1][0] - cella_merete
+            jatek_vege = jateknak_vege(jatek_vege)
 
-    if kigyo_frissit > 170:
-        kigyo_frissit = 0
-        kigyo_helye = kigyo_helye[-1:] + kigyo_helye[:-1]
-        #fel
-        if irany == 1:
-            kigyo_helye[0][0] = kigyo_helye[1][0]
-            kigyo_helye[0][1] = kigyo_helye[1][1] - cella_merete
-        #jobbra
-        if irany == 2:
-            kigyo_helye[0][1] = kigyo_helye[1][1]
-            kigyo_helye[0][0] = kigyo_helye[1][0] + cella_merete
-        #le
-        if irany == 3:
-            kigyo_helye[0][0] = kigyo_helye[1][0]
-            kigyo_helye[0][1] = kigyo_helye[1][1] + cella_merete
-        #balra
-        if irany == 4:
-            kigyo_helye[0][1] = kigyo_helye[1][1]
-            kigyo_helye[0][0] = kigyo_helye[1][0] - cella_merete
+    if jatek_vege == True:
+        jatek_vege_kirajzolasa()
+        if event.type == pygame.MOUSEBUTTONDOWN and kattintva == False:
+            kattintva = True
+        if event.type == pygame.MOUSEBUTTONUP and kattintva == True:
+            kattintva = False
+            hely = pygame.mouse.get_pos()
+            if ujra_doboz.collidepoint(hely):
+            #Újra megcsinálja a kígyót, és a változókat.
+                irany = 1
+                kigyo_frissit = 0
+                kaja = [0, 0]
+                uj_kaja = True
+                uj_darab = [0, 0]
+                pontszam = 0
+                jatek_vege = False
+
+                kigyo_helye = [[int(screen_width / 2), int(screen_height / 2)]]
+                kigyo_helye.append([int(screen_width / 2), int(screen_height / 2) + cella_merete])
+                kigyo_helye.append([int(screen_width / 2), int(screen_height / 2) + cella_merete * 2])
+                kigyo_helye.append([int(screen_width / 2), int(screen_height / 2) + cella_merete * 3])
 
 #Kígyó megcsinálása
     fej = 1
