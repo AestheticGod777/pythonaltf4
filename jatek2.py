1afa493fa19508f56b2e1b2d7b7dbba4c8f98f4a
feketefelix555 <fekete.felix2005@gmail.com>
2021-06-02 12:25:11 +0200

    asdizuzki

--- jatek2.py
+++ jatek2.py
@@ -4,13 +4,6 @@ import random
 
 class GameStage(MyStage):
 
-    grid_x_count = 20
-    grid_y_count = 15
-    food_position = {
-        'x': random.randint(0, grid_x_count - 1),
-        'y': random.randint(0, grid_y_count - 1),
-    }
-
     lastkey = None
 
     def back(self, pos, btn):
@@ -26,29 +19,33 @@ class GameStage(MyStage):
 
     def __init__(self, menu: 'Menustage'):
         super().__init__()
-        self.b: MyActor = MyActor("ama.png", pos=(200, 200), anchor=(16, 16))
-        self.add_actor(self.b)
+        self.food: MyActor = MyActor("ama.png", pos=(200, 200), anchor=(16, 16))
+        self.add_actor(self.food)
         self.set_on_key_down_listener(self.keydownlistener)
         self.set_on_key_up_listener(self.keyuplistener)
-        self.m: MyActor = MyActor("kocka.png", pos=(300, 300), anchor=(16, 16))
-        self.add_actor(self.m)
+        self.snake: MyActor = MyActor("kocka.png", pos=(300, 300), anchor=(16, 16))
+        self.add_actor(self.snake)
 
     def update(self, deltaTime: float = 0.0166666666666666666666):
         super().update(deltaTime)
         if self.lastkey != None:
             if self.lastkey == keys.UP:
-                animate(self.m, pos=(self.m.pos[0], self.m.pos[1] - 1000), duration=4)
-                self.m.set_rotation(360)
+                animate(self.snake, pos=(self.snake.pos[0], self.snake.pos[1] - 1000), duration=4)
+                self.snake.set_rotation(360)
             if self.lastkey == keys.DOWN:
-                animate(self.m, pos=(self.m.pos[0], self.m.pos[1] + 1000), duration=4)
-                self.m.set_rotation(180)
+                animate(self.snake, pos=(self.snake.pos[0], self.snake.pos[1] + 1000), duration=4)
+                self.snake.set_rotation(180)
             if self.lastkey == keys.LEFT:
-                animate(self.m, pos=(self.m.pos[0] - 1000, self.m.pos[1]), duration=4)
-                self.m.set_rotation(90)
+                animate(self.snake, pos=(self.snake.pos[0] - 1000, self.snake.pos[1]), duration=4)
+                self.snake.set_rotation(90)
             if self.lastkey == keys.RIGHT:
-                animate(self.m, pos=(self.m.pos[0] + 1000, self.m.pos[1]), duration=4)
-                self.m.set_rotation(270)
-        global food_position
+                animate(self.snake, pos=(self.snake.pos[0] + 1000, self.snake.pos[1]), duration=4)
+                self.snake.set_rotation(270)
+
+        if self.food.is_on_stage() and self.food.overlaps_with(self.snake):
+            self.food.remove_from_stage()
+
+
 
 
 
