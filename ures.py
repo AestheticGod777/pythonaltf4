11c929693f3c1757e8eef519f8831882a334644a
Félix <fekete.felix2005@gmail.com>
2021-06-05 23:34:47 +0200

    bruh

--- ures.py
+++ ures.py
@@ -11,7 +11,7 @@ class BlankStage(MyStage):
 
     def __init__(self, menu: 'Menustage'):
         super().__init__()
-        self.bruh: MyActor = MyActor("bruh.png", pos=(200, 100), anchor=(1, 1))
+        self.bruh: MyActor = MyActor("bruhu.png", pos=(200, 100), anchor=(1, 1))
         self.add_actor(self.bruh)
         self.menu = menu
         self.set_on_key_down_listener(self.keydown)
