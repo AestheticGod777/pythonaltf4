2e024c66c0f741f7ca3cadb02653043d014d0940
kistapir1 <kistapir0@gmail.com>
2021-06-08 14:03:23 +0200

    Blank menü megcsinálva, low quality képekkel

--- ures.py
+++ ures.py
@@ -3,6 +3,18 @@ from n_menu_menustage import *
 
 class BlankStage(MyStage):
 
+    def menu_Main(self, pos=0, btn=0):
+        self.onscreenstage = self
+
+    def menu_Game(self, pos=0, btn=0):
+        self.onscreenstage = GameStage(self)
+
+    def menu_Blank(self, pos=0, btn=0):
+        self.onscreenstage = BlankStage(self)
+
+    def menu_Exit(self, pos=0, btn=0):
+        exit()
+
     def keydown(self, key, mod, unicode):
         print(key)
         if key == keys.ESCAPE:
@@ -11,7 +23,14 @@ class BlankStage(MyStage):
 
     def __init__(self, menu: 'Menustage'):
         super().__init__()
-        self.bruh: MyActor = MyActor("bruhu.png", pos=(200, 100), anchor=(1, 1))
-        self.add_actor(self.bruh)
         self.menu = menu
-        self.set_on_key_down_listener(self.keydown)
+
+        menuitem1: MyActor = MyActor("restart.png", pos=(100, 100), anchor=(0, 0))
+        self.add_actor(menuitem1)
+        menuitem1.set_on_mouse_down_listener(self.menu.menu_Game)
+
+        menuitem2: MyActor = MyActor("menu.png", pos=(100, 250), anchor=(0, 0))
+        self.add_actor(menuitem2)
+        menuitem2.set_on_mouse_down_listener(self.menu.menu_Main)
+
+
